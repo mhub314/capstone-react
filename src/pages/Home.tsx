501f388b3850121520c173bab5941ab4fbc38db9
Mary Hubert <mhubert314@gmail.com>
2024-04-16 14:01:14 -0700

    fixing image path

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,13 @@
-import Background from  "../assets/images/concert.jpg"
+import backgroundImage from "../assets/images_1/cheering-crowd-at-a-concert--805394354-59e62cadb501e80011bbb996.jpg"
 
 function Home() {
   return (
     <div 
-      style={{ backgroundImage: `url(${ Background })`}} 
+      style={{ backgroundImage: `url(${ backgroundImage })`}} 
       className='flex flex-row justify-center mx-auto bg-cover bg-fixed'
       >
-        <div className=' flex place-items-center h-screen'>
-          <h3 className='font-serif p-8 mb-16 text-2xl bg-gray-900 bg-opacity-20 text-neutral-100 rounded'>Compose, Collaborate, Conquer: Your Songwriting Oasis</h3>
+        <div className='flex place-items-center h-screen'>
+          <h3 className='font-serif p-8 mb-16 text-5xl font-bold text-neutral-900 rounded'>Compose, Collaborate, Conquer: Your Songwriting Oasis</h3>
         </div>
     </div>
   )
